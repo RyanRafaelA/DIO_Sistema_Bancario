9866029a7b61da56dc82dc199a6ce13535e0492e
RyanRafaelA <ryrafael461@gmail.com>
2024-07-02 16:27:24 -0300

    Inicializando o projeto

---
+++ desafio.py
@@ -0,0 +1,33 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+
+=>"""
+
+saldo = 0
+limite= 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu).lower()
+
+    if opcao == "d":
+        print("Depósito")
+    
+    elif opcao == "s":
+        print("Sacar")
+    
+    elif opcao == "e":
+        print("Extrato")
+    
+    elif opcao == "s":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
