a89b931a8f58e1de9567a9a1e6650ef4d89d6df6
RyanRafaelA <ryrafael461@gmail.com>
2024-07-04 22:03:13 -0300

    Criação da função main e a menu

--- desafio.py
+++ desafio.py
@@ -1,3 +1,15 @@
+def menu():
+    menu = """\n
+==================MENU==================
+[d]  Depositar
+[s]  Sacar
+[e]  Extrato
+[lc] Listar Contas
+[nu] Novo Usuáiro
+[q]  Sair\n
+=> """
+    return input(menu)
+
 def depositar(valor):
     if valor<=0 :
         print("O valor informado não pode ser menor ou igual a 0")
@@ -5,58 +17,46 @@ def depositar(valor):
 
     return valor
 
-menu = """
+def main():
+    saldo = 0
+    limite= 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+    valor = 0
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    while True:
+        opcao = menu()
 
+        if opcao == "d":
+            valor = float(input("Depósito\nR$ "))
+            extrato += f"Depósito: R${valor:.2f}\n"
+            saldo += depositar(valor)
+        elif opcao == "s":
+            sacar = float(input("Sacar\nR$ "))
 
-=>"""
+            if numero_saques < LIMITE_SAQUES:
+                if saldo >= 0 and saldo >= sacar:
+                    if sacar > 0 and sacar <= limite:
+                        saldo -= sacar
+                        numero_saques+=1
 
-saldo = 0
-limite= 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
-valor = 0
-
-while True:
-    opcao = input(menu).lower()
-
-    if opcao == "d":
-        valor = float(input("Depósito\nR$ "))
-
-        extrato += f"Depósito: R${valor:.2f}\n"
-
-        saldo += depositar(valor)
-
-        
-    
-    elif opcao == "s":
-        sacar = float(input("Sacar\nR$ "))
-
-        if numero_saques < LIMITE_SAQUES:
-            if saldo >= 0 and saldo >= sacar:
-                if sacar > 0 and sacar <= limite:
-                    saldo -= sacar
-                    numero_saques+=1
-
-                    extrato += f"Saque: R${sacar:.2f}\n"
+                        extrato += f"Saque: R${sacar:.2f}\n"
+                    else:
+                        print("Não pode sacar 0 ou maior que 500 reais.")
                 else:
-                    print("Não pode sacar 0 ou maior que 500 reais.")
+                    print("Não pode sacar")
             else:
-                print("Não pode sacar")
-        else:
-            print("O numero de saques não pode ser maior que 3")
+                print("O numero de saques não pode ser maior que 3")
     
-    elif opcao == "e":
-        print("Extrato: ")
-        print(f"{extrato} \nSaldo final: R${saldo:.2f}")
+        elif opcao == "e":
+            print("Extrato: ")
+            print(f"{extrato} \nSaldo final: R${saldo:.2f}")
     
-    elif opcao == "q":
-        break
+        elif opcao == "q":
+            break
 
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+            
+main()
