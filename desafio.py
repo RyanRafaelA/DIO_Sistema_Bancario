e6d8b4bda8148ee66f5308e9b45c8feba5885656
RyanRafaelA <ryrafael461@gmail.com>
2024-07-12 19:04:23 -0300

    Inicializando a classe Saque

--- desafio.py
+++ desafio.py
@@ -124,6 +124,14 @@ class Transacao(ABC):
     
     @abstractmethod
     def registrar(self, conta): pass
+    
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property    
+    def valor(self):
+        return self._valor
                
 def menu():
     menu = """\n
